f0d1f221101e93a8096f197d09de4b094112de41
Rafa <rafaelfigueiredojunior7@gmail.com>
2022-06-16 17:57:07 -0300

    first commit

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+    API_URL: 'http://localhost:5000',
+    MONGO_CONNECTION: 
+    'mongodb+srv://rafa:<password>@cluster0.njsudac.mongodb.net/?retryWrites=true&w=majority'
+}
